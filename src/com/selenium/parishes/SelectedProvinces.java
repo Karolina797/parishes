e0e61a62ec353a8d381506b7484b1577d246b602
cristofp <krzysiekplachno@gmail.com>
2019-11-07 23:44:27 +0100

    komentarze

--- src/com/selenium/parishes/SelectedProvinces.java
+++ src/com/selenium/parishes/SelectedProvinces.java
@@ -25,7 +25,7 @@ public class SelectedProvinces {
         System.setProperty("webdriver.chrome.driver", "C:\\Work\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-        List<Parish> parishesList = new LinkedList<>();
+        List<Parish> parishesList = new LinkedList<>(); // inicjalizację tej listy możemy przenieść do parishesFromAllProvince
 
 
         //Array of provinces
@@ -41,8 +41,8 @@ public class SelectedProvinces {
     public static void parishData(WebDriver driver, List parishesList) {
 
         WebDriverWait w = new WebDriverWait(driver, 15);
-        w.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@class='prawa2']/div[2]")));
-        driver.findElement(By.xpath("//div[@class='prawa2']/div[2]"));
+        w.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@class='prawa2']/div[2]"))); //to jest niepotrzebne, i zobacz, że ten div ogólnie nie jest klikalny
+        driver.findElement(By.xpath("//div[@class='prawa2']/div[2]")); //ale już to jest potrzebne
         Parish parish = new Parish(driver.findElement(By.xpath("//div[@class='item']/h1")).getText(), driver.findElement(By.xpath("//div[@class='prawa2']/div[2]")).getText(),
                 driver.findElement(By.xpath("//div[@class='prawa2']/div[4]")).getText(), driver.findElement(By.xpath("//div[@class='prawa2']/div[6]")).getText(),
                 driver.findElement(By.xpath("//div[@class='prawa2']/div[8]")).getText(), driver.findElement(By.xpath("//div[@class='prawa2']/div[10]")).getText(),
@@ -68,7 +68,7 @@ public class SelectedProvinces {
 
             try {
                 w.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@class='prawa2']/div[2]")));
-            } catch (Exception exception) {
+            } catch (Exception exception) { //myślę, że w momencie kiedy już dowiedzieliśmy się, że odpowiednim checkiem jest `elementToBeClickable`
                 driver.findElements(By.cssSelector("a[href*='/parafia-']")).get(i).click();
                 w.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@class='prawa2']/div[2]")));
             }
@@ -81,9 +81,9 @@ public class SelectedProvinces {
     //Data of parishes from one province
     public static void parishesFromOneProvince(WebDriver driver, List parishesList, String currentProvince) throws IOException {
 
-        driver.findElement(By.cssSelector("a[title='ostatnia']")).click();
+        driver.findElement(By.cssSelector("a[title='ostatnia']")).click(); //tu też powinniśmy najpier explicite poczekać na klikalność tego elemetnu zanim klikniemy
         int numberOfPages = Integer.parseInt(driver.findElement(By.cssSelector("span.nawigacja_a")).getText());
-        driver.findElement(By.linkText("«")).click();
+        driver.findElement(By.linkText("«")).click(); //j.w.
 
         int i = 0;
         while (i < numberOfPages) {
@@ -91,6 +91,7 @@ public class SelectedProvinces {
             parishesFromOnePage(driver, parishesList);
 
             if (i == numberOfPages - 1) {
+                //całą zawartość tego bloku powinniśmy wyeksportować do funkcji exportParishesToCsv(parishesList, csvFileName)
                 try (
                         BufferedWriter writer = Files.newBufferedWriter(Paths.get("./" + currentProvince + ".csv"));
                         CSVPrinter csvPrinter = new CSVPrinter(writer, CSVFormat.DEFAULT.withHeader("Parish Name", "Address", "Phone Number", "url", "Diocese", "Decanate", "Notes"));
@@ -99,7 +100,7 @@ public class SelectedProvinces {
                         csvPrinter.printRecord(((Parish) parishesList.get(t)).getName(), ((Parish) parishesList.get(t)).getAddress().replaceAll("\n", " "), ((Parish) parishesList.get(t)).getPhoneNumber(),
                                 ((Parish) parishesList.get(t)).getUrl(), ((Parish) parishesList.get(t)).getDiocese(), ((Parish) parishesList.get(t)).getDecanate(), ((Parish) parishesList.get(t)).getNotes().replaceAll("\n", " "));
                     }
-                    csvPrinter.flush();
+                    csvPrinter.flush(); //wydaje mi się, że to jest zbędne
                 }
 
             } else {
@@ -116,7 +117,7 @@ public class SelectedProvinces {
 
         driver.get("http://www.plebanie.pl/");
         int provincesSize = driver.findElements(By.cssSelector("div.item_left a[href*='/wojewodztwo/']")).size();
-        List provincesList = Arrays.asList(provinces);
+        List provincesList = Arrays.asList(provinces); //tą konwersję z array na list możemy zrobić poziom wyżej w mainie i do tej metody parishesFromAllProvince przekazać już listę
 
         int j = 0;
 
@@ -125,10 +126,10 @@ public class SelectedProvinces {
             String currentProvince = driver.findElements(By.cssSelector("div.item_left a[href*='/wojewodztwo/']")).get(i).getText();
 
             if (provincesList.contains(currentProvince)) {
-                driver.findElements(By.cssSelector("div.item_left a[href*='/wojewodztwo/']")).get(i).click();
+                driver.findElements(By.cssSelector("div.item_left a[href*='/wojewodztwo/']")).get(i).click(); //w sumie dla 100% pewnosci powinnićmy najpierw poczekać na klikalność tego elementu zanim klikniemy
                 parishesFromOneProvince(driver, parishesList, currentProvince);
                 j++;
-                if (j == provinces.length)
+                if (j == provinces.length) //moim zdaniem wprowadzenie parametru j tylko po to, żeby po przerobieniu ostatniej z zadany prowincji od razu skończyć pętlę to zbędna optymalizacja. Wyrzucając zmienną j bardzo uprościmy kod
                     break;
             }
         }
